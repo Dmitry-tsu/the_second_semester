869b3fa41342d5152b66f69e1a7291e7cd9901f2
Dmitry <dmitriy.tartychnyy@stud.tsu.ru>
2024-04-11 02:47:56 +0700

    создание проекта

---
+++ Binary_Tree/Binary_Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Дмитрий Тартычный on 11.04.2024.
+//
+
+#include "Binary_Tree.h"
---
+++ Binary_Tree/Binary_Tree.h
@@ -0,0 +1,14 @@
+//
+// Created by Дмитрий Тартычный on 11.04.2024.
+//
+
+#ifndef THE_SECOND_SEMESTER_BINARY_TREE_H
+#define THE_SECOND_SEMESTER_BINARY_TREE_H
+
+
+class Binary_Tree {
+
+};
+
+
+#endif //THE_SECOND_SEMESTER_BINARY_TREE_H
